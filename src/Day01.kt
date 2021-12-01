2ecae012c0292fc957264e77fc03b917f0d83e22
Ajesh Nair6 <anair6@lenovo.com>
2021-12-01 23:17:02 +0530

    number of measurements larger than the previous measurement

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var noOfLargerMeasurements = 0
+        input.reduce { depth, nextDepth ->
+            if (depth.toInt() < nextDepth.toInt()) noOfLargerMeasurements++
+            return@reduce nextDepth
+        }
+        return noOfLargerMeasurements
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowSize = 3
+        val measuredWindows = input.windowed(windowSize, 1) { depths ->
+            depths.map { it.toInt() }.sum().toString()
+        }
+        return part1(measuredWindows)
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
